9cc0778655473633739c027c765e0e3ea0c648ce
Bruno <omarjm0910@gmail.com>
2022-06-09 16:27:56 -0500

    l

--- server.c
+++ server.c
@@ -42,8 +42,8 @@ void serve(int s) {
                 strcpy(rutaAMandar,token+1);
                 strcpy(rutaAMandar2,token+1);
                 printf("LA RUTA A MANDAR ES: %s\n",rutaAMandar);
-                if(rutaAMandar == "/"){
-                    strcpy(rutaAMandar,"listado.txt");
+                if(rutaAMandar == "listado.txt"){
+                    printf("Imprimimos el listado de archivos+++");
                 }
             }
             indice++;
