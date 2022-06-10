cf127291b866020b70a2d52bab69ba0e8e2034ce
Bruno <omarjm0910@gmail.com>
2022-06-09 19:51:41 -0500

    k

--- server.c
+++ server.c
@@ -16,7 +16,26 @@
 #define MSGSIZE 1024
 #define FILE_TO_SEND "index.html"
 void listarArchivos(){
-    printf("Hablando desde la funcion de listar archivos");
+    char *filename = "listado.txt";
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL)
+    {
+        printf("Error opening the file %s", filename);
+        return -1;
+    }
+    struct dirent *de;
+	DIR *dr = opendir(".");
+	if (dr == NULL) 
+	{
+        printf("Error opening the file %s", filename);
+		printf("Could not open current directory" );
+		return 0;
+	}
+	while ((de = readdir(dr)) != NULL){
+            fprintf(fp, "%s\n", de->d_name);
+    }
+    fclose(fp);
+	closedir(dr);	
 }
 void serve(int s) {
     char buffer[MSGSIZE];
